4ba2b92aecef19104745fec881d79747e9e8bfbe
stefantanasa <liketanasa@gmail.com>
2022-01-21 08:18:25 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function App() {
           <Route path="*" element={<Error404 />} />
 
           <Route
-            path="/"
+            path="/me"
             element={<ProfilePage name={name} surname={surname} />}
           />
         </Routes>
